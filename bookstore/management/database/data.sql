bda15ff9628a3ddad5873111e9298d9f8970128b
Atom M. Nava <atommunoznava@gmail.com>
2025-04-05 08:05:30 -0600

    Update data.sql

--- bookstore/management/database/data.sql
+++ bookstore/management/database/data.sql
@@ -1,4 +1,4 @@
 INSERT INTO books (title, author, genre, price, stock, cover_image) VALUES
-('El Principito', 'Antoine de Saint-Exupéry', 'Ficción', 12.99, 10, 'covers/principito.jpg'),
-('Cien Años de Soledad', 'Gabriel García Márquez', 'Realismo Mágico', 15.50, 5, 'covers/cien_anos.jpg'),
-('1984', 'George Orwell', 'Distopía', 14.99, 8, 'covers/1984.jpg');
+('El Principito', 'Antoine de Saint-Exupéry', 'Ficción', 12.99, 10, 'covers/principito.png'),
+('Cien Años de Soledad', 'Gabriel García Márquez', 'Realismo Mágico', 15.50, 5, 'covers/cien_anios.png'),
+('1984', 'George Orwell', 'Distopía', 14.99, 8, 'covers/1984.png');
